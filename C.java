645dcced47f18c359f9eaa5a54d7383764b37a93
Saariya <sariya8297@gmail.com>
2026-07-27 02:37:40 +0530

    Static variable

--- C.java
+++ C.java
@@ -1,6 +1,18 @@
 public class C {
-    static int x = 10;//Static variable are created out side the main method with static keyword and belongs to class(means can only be accessed by class name).
+    static int x = 10;//Static variable are created out side the main method with static keyword and belongs to class
+    // // (means can only be accessed by class name).
+    //static variables has a global access.
     public static void main(String[] args){
+       // System.out.println(C.x);
+       C c= new C();
+       c.test();
+
+
+    }
+    public void test(){
         System.out.println(C.x);
     }
 }
+    
+
+
