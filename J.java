ad743d66ea6fd364e2a31f744c56189ce8d948dd
Saariya <sariya8297@gmail.com>
2026-07-28 16:50:31 +0530

    Construtors overloading

--- J.java
+++ J.java
@@ -18,3 +18,8 @@ J j1= new J(10);
 J j2 = new J(10, 'c');
     }
 }
+//Default construtor  - If you don't write any constructor, 
+// Java silently provides an empty one for you at the compilation time with no arguments.
+//Default construtor is not appilcable for object with arguments.
+//If in the same program object with and without argument is created then default construtor is not applicable.
+// (Java automatically gives you a default no-argument constructor — but only if you haven't written any constructor at all in your class. The moment you write even one constructor (with or without parameters), Java stops providing that automatic default one.)
